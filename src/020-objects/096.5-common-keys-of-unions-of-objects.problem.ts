1f0019dd740836a8f41289f15ec4719cec23a197
Dieguinho Pinho <diegocuehdz@gmail.com>
2024-12-28 23:12:56 -0600

    feat: finish objects

--- src/020-objects/096.5-common-keys-of-unions-of-objects.problem.ts
+++ src/020-objects/096.5-common-keys-of-unions-of-objects.problem.ts
@@ -21,7 +21,9 @@ type Product = {
   imageId: string;
 };
 
-const getAvatarImage = (entity: unknown) => {
+type SharedEntity = User | Organisation | Product;
+
+const getAvatarImage = (entity: SharedEntity) => {
   {
     // Should not be able to access properties that are
     // not common to both types
