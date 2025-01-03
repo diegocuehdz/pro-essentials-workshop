c583ddf3459a513a8192921ad2e507b87d457625
Dieguinho Pinho <diegocuehdz@gmail.com>
2025-01-03 01:14:37 -0600

    feat: first batch of configuring typescript

--- src/065-types-you-dont-control/174.5-modifying-window.problem/src/index.ts
+++ src/065-types-you-dont-control/174.5-modifying-window.problem/src/index.ts
@@ -1,5 +1,14 @@
 import { Equal, Expect } from "@total-typescript/helpers";
 
-const state = window.DEBUG.getState();
+declare global {
+  interface Window {
+    DEBUG: {
+      getState: () => {
+        id: string;
+      };
+    };
+  }
+}
 
+const state = window.DEBUG.getState();
 type test = Expect<Equal<typeof state, { id: string }>>;
